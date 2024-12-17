6ab626accc870ebb501a96868d5a81a38fcfe614
Bharat <b1997oct@gmail.com>
2024-12-17 10:27:48 +0530

    2nd Commit

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -3,12 +3,14 @@ import React from 'react'
 
 export default function NavBar({ title }) {
     return (
-        <div className="flex gap-4 shadow p-6">
+        <div className="flex gap-8 shadow py-4 px-6 items-center">
             <div className="flex-1">
+                <a href='/'>
                 <h3 className="font-bold text-xl">{title}</h3>
+                </a>
             </div>
-            <Link href={'/account'}>Signup</Link>
-            <Link href={'/account'}>Login</Link>
+            <Link href={'/login'}>Login</Link>
+            <Link className='bg-black rounded-lg text-white px-4 py-2' href={'/signup'}>Signup</Link>
         </div>
     )
 }
